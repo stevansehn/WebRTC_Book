c0a143e9d686384742a794bbbfa23d17688507c0
Stevan Sehn <stevanlinse@gmail.com>
2020-08-11 08:27:57 -0300

    simple server ok

--- SimpleNodeServer_OReilly_Review/simpleNodeServer_OK.js
+++ SimpleNodeServer_OReilly_Review/simpleNodeServer_OK.js
@@ -1,105 +1,120 @@
-var static = require('node-static');
+var static = require("node-static");
 
-var http = require('http');
+var port = 8181;
+
+var http = require("http");
 
 // Create a node-static server instance listening on port 8181
-var file = new(static.Server)();
+var file = new static.Server();
 
 // We use the http moduleÕs createServer function and
 // use our instance of node-static to serve the files
-var app = http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8181);
+var app = http
+  .createServer(function (req, res) {
+    file.serve(req, res);
+  })
+  .listen(port);
 
-console.log('Listening on ' + app.address().port);
+console.log("Listening on " + app.address().port);
 
 // Use socket.io JavaScript library for real-time web applications
-var io = require('socket.io').listen(app);
-
+var io = require("socket.io").listen(app);
 
 // credits to http://stackoverflow.com/questions/6563885/socket-io-how-do-i-get-a-list-of-connected-sockets-clients/24145381#24145381
 function findClientsSocket(roomId, namespace) {
-    var res = [];
-    var ns = io.of(namespace ||"/");    // the default namespace is "/"
+  var res = [];
+  var ns = io.of(namespace || "/"); // the default namespace is "/"
 
-    if (ns) {
-        for (var id in ns.connected) {
-            if(roomId) {
-                var index = ns.connected[id].rooms.indexOf(roomId) ;
-                if(index !== -1) {
-                    res.push(ns.connected[id]);
-                }
-            } else {
-                res.push(ns.connected[id]);
-            }
+  if (ns) {
+    for (var id in ns.connected) {
+      if (roomId) {
+        var index = ns.connected[id].rooms.indexOf(roomId);
+        if (index !== -1) {
+          res.push(ns.connected[id]);
         }
+      } else {
+        res.push(ns.connected[id]);
+      }
     }
-    return res.length;
+  }
+  return res.length;
 }
 
 // Let's start managing connections...
-io.sockets.on('connection', function (socket){
+io.sockets.on("connection", function (socket) {
 
-		// Handle 'message' messages
-        socket.on('message', function (message) {
-                log('S --> Got message: ', message);
-                socket.broadcast.to(message.channel).emit('message', message.message);
-        });
+  // Mensagem customizada
+  socket.emit("boas-vindas", "Olá, Cliente!");
 
-		// Handle 'create or join' messages
-        socket.on('create or join', function (channel) {
-                var numClients = findClientsSocket(channel);
-                console.log('numclients = ' + numClients);
+  // Handle 'message' messages
+  socket.on("message", function (message) {
+    log("S --> Got message: ", message);
+    socket.broadcast.to(message.channel).emit("message", message.message);
+  });
 
-                // First client joining...
-                if (numClients == 0){
-                        socket.join(channel);
-                        socket.emit('created', channel);
-                // Second client joining...
-                } else if (numClients == 1) {
-                        // Inform initiator...
-                		io.sockets.in(channel).emit('remotePeerJoining', channel);
-                		// Let the new peer join channel
-                        socket.join(channel);
+  // Handle 'create or join' messages
+  socket.on("create or join", function (channel) {
+    var numClients = findClientsSocket(channel);
+    console.log("numclients = " + numClients);
 
-                        socket.broadcast.to(channel).emit('broadcast: joined', 'S --> \
-                        		broadcast(): client ' + socket.id + ' joined channel ' + channel);
-                } else { // max two clients
-                		console.log("Channel full!");
-                        socket.emit('full', channel);
-                }
-        });
+    // First client joining...
+    if (numClients == 0) {
+      socket.join(channel);
+      socket.emit("created", channel);
+      // Second client joining...
+    } else if (numClients == 1) {
+      // Inform initiator...
+      io.sockets.in(channel).emit("remotePeerJoining", channel);
+      // Let the new peer join channel
+      socket.join(channel);
 
-        // Handle 'response' messages
-        socket.on('response', function (response) {
-            log('S --> Got response: ', response);
+      socket.broadcast
+        .to(channel)
+        .emit(
+          "broadcast: joined",
+          "S --> \
+                        		broadcast(): client " +
+            socket.id +
+            " joined channel " +
+            channel
+        );
+    } else {
+      // max two clients
+      console.log("Channel full!");
+      socket.emit("full", channel);
+    }
+  });
 
-            // Just forward message to the other peer
-            socket.broadcast.to(response.channel).emit('response', response.message);
-        });
+  // Handle 'response' messages
+  socket.on("response", function (response) {
+    log("S --> Got response: ", response);
 
-        // Handle 'Bye' messages
-        socket.on('Bye', function(channel){
-        	// Notify other peer
-        	socket.broadcast.to(channel).emit('Bye');
+    // Just forward message to the other peer
+    socket.broadcast.to(response.channel).emit("response", response.message);
+  });
 
-        	// Close socket from server's side
-        	socket.disconnect();
-        });
+  // Handle 'Bye' messages
+  socket.on("Bye", function (channel) {
+    // Notify other peer
+    socket.broadcast.to(channel).emit("Bye");
 
-        // Handle 'Ack' messages
-        socket.on('Ack', function () {
-            console.log('Got an Ack!');
-            // Close socket from server's side
-        	socket.disconnect();
-        });
+    // Close socket from server's side
+    socket.disconnect();
+  });
 
-		// Utility function used for remote logging
-		function log(){
-			var array = [">>> "];
-			for (var i = 0; i < arguments.length; i++) {
-				array.push(arguments[i]);
-			}
-			socket.emit('log', array);
-		};
+  // Handle 'Ack' messages
+  socket.on("Ack", function () {
+    console.log("Got an Ack!");
+    // Close socket from server's side
+    socket.disconnect();
+  });
+
+  // Utility function used for remote logging
+  function log() {
+    var array = [">>> "];
+    for (var i = 0; i < arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+    socket.emit("log", array);
+  }
 });
